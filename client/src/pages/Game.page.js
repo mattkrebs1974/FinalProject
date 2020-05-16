10e2fcb73045f08747c0bdc73c78f73849bcf9e4
Jacob Gold <jkobygold@gmail.com>
2020-05-16 18:03:06 -0400

    adding game

--- client/src/pages/Game.page.js
+++ client/src/pages/Game.page.js
@@ -1,29 +1,70 @@
 import React, { Component } from "react";
 
+
+
+var click= 0;
+function rando(){
+return Math.floor(Math.random()*200)+200;
+}
+
+  
 export default class Home extends Component {
+
+
+   
+
+
   constructor(props) {
   
     super(props);
-    this.state = { x: 0, y: 0, hidden: false };
+    this.state = {
+       x: 0,
+        y: 0, 
+        hidden: false,
+        startTime: 0,
+       };
   }
-  
+
+
   move() {
+    
+     click++
+     console.log(click);
     this.setState(state => ({
-      x: state.x + 5,
-      y: state.y + 10,
+      x:  rando(),
+      y:  rando(),
+      userResult:  (Date.now() -this.state.startTime)/ 1000,
+      startTime: Date.now(),
       hidden: true
     }));
-    setTimeout(() => {
+    const timeout = setTimeout(() => {
       this.setState(state => ({
         hidden: false
       }));
     }, 1000)
+
+    if (click===5){
+    clearTimeout(timeout)
+  
+   }
   }
   
-  
+
+
+componentDidMount(){
+  this.setState(
+   {
+     startTime: Date.now()
+   })
+}
+
+
 
   render() {
+    console.log("x:", this.state.x)
+    console.log("y:", this.state.y)
     return (
+      <div >
       <div style={{position: 'relative', height: 500}}>
         <div style={{
           position: 'absolute',
@@ -37,6 +78,11 @@ export default class Home extends Component {
         onClick={() => {this.move( 
               )}}
         ></div>
+        <h1> Your results: {this.state.userResult} seconds </h1>
+        {click===5? <a href = "http://localhost:3000/Game"> <img src= 'https://www.freepnglogos.com/uploads/button-png/red-button-circle-image-pixabay-20.png' style={{ width: 400, height: 400}} /> </a>: null } 
+        {click===5? <h1> Please select giant red button to proceed</h1>: null } 
+
+      </div>
       </div>
     );
   }
