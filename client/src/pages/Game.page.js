b4f8f7cdd6e5adc476a9c4027b5e5ad3d1583122
Jacob Gold <jkobygold@gmail.com>
2020-05-17 15:55:36 -0400

    data configured and added to database

--- client/src/pages/Game.page.js
+++ client/src/pages/Game.page.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 
 var counter = 0
-var click= 0;
-function rando(){
-return Math.floor(Math.random()*400)+200;
+var click = 0;
+function rando() {
+  return Math.floor(Math.random() * 400) + 200;
 }
 
 function getRandomColor() {
@@ -12,41 +13,43 @@ function getRandomColor() {
   var letters = "0123456789ABCDEF".split('');
   var color = "#";
   for (var i = 0; i < 6; i++) {
-      color += letters[Math.round(Math.random() * 15)];
+    color += letters[Math.round(Math.random() * 15)];
   } //ends for loop 
   return color;
 
 }
-  
+
 export default class Home extends Component {
 
 
-   
+
 
 
   constructor(props) {
-  
+
     super(props);
     this.state = {
-       x: 0,
-        y: 0, 
-        hidden: false,
-        startTime: 0,
-        score: 0
-       };
+      x: 0,
+      y: 0,
+      hidden: false,
+      startTime: 0,
+      score: 0,
+      userResult: [],
+    };
   }
 
 
   move() {
-    
-     click++
-     console.log(click);
+    var temparray = this.state.userResult;
+    temparray.push((Date.now() - this.state.startTime) / 1000);
+    click++
+    console.log(click);
     this.setState(state => ({
-      x:  rando(),
-      y:  rando(),
-      userResult:  (Date.now() -this.state.startTime)/ 1000,
+      x: rando(),
+      y: rando(),
+      userResult: temparray,
       startTime: Date.now(),
-      hidden: true, 
+      hidden: true,
       score: this.state.score + trackScore
     }));
     const timeout = setTimeout(() => {
@@ -55,23 +58,33 @@ export default class Home extends Component {
       }));
     }, 1000)
 
-    if (click===8){
-    clearTimeout(timeout)
+    if (click === 8) {
+      clearTimeout(timeout)
+      axios.post('/api/score', {
+        score: this.state.score,
+        userResult: this.state.userResult
+      })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
 
-   }
-var trackScore = (Date.now() -this.state.startTime)/ 1000
+    }
+    var trackScore = (Date.now() - this.state.startTime) / 1000
   }
-   
-
- 
 
 
-componentDidMount(){
-  this.setState(
-   {
-     startTime: Date.now()
-   })
-}
+
+
+
+  componentDidMount() {
+    this.setState(
+      {
+        startTime: Date.now()
+      })
+  }
 
 
 
@@ -80,26 +93,28 @@ componentDidMount(){
     console.log("y:", this.state.y)
     return (
       <div >
-      <div style={{position: 'relative', height: 500}}>
-        <div style={{
-          position: 'absolute',
-          visibility: this.state.hidden ? 'hidden' : 'visible',
-          top: this.state.y,
-          right: this.state.x,
-          height: 100,
-          width: 100,
-          borderradius: 50,
-          backgroundColor: getRandomColor()
-        }}
-        onClick={() => {this.move( 
-              )}}
-        ></div>
-        <h1> Your results: {this.state.userResult} seconds </h1>
-        <h2>Your composite score: {this.state.score}</h2>
-        {click===8? <a href = "http://localhost:3000/Game"> <img src= 'https://www.freepnglogos.com/uploads/button-png/red-button-circle-image-pixabay-20.png' style={{ width: 400, height: 400}} /> </a>: null } 
-        {click===8? <h1> Please select giant red button to proceed</h1>: null } 
+        <div style={{ position: 'relative', height: 500 }}>
+          <div style={{
+            position: 'absolute',
+            visibility: this.state.hidden ? 'hidden' : 'visible',
+            top: this.state.y,
+            right: this.state.x,
+            height: 100,
+            width: 100,
+            borderradius: 50,
+            backgroundColor: getRandomColor()
+          }}
+            onClick={() => {
+              this.move(
+              )
+            }}
+          ></div>
+          <h1> Your results: {this.state.userResult[this.state.userResult.length - 1]} seconds </h1>
+          <h2>Your composite score: {this.state.score}</h2>
+          {click === 8 ? <a href="http://localhost:3000/Game"> <img src='https://www.freepnglogos.com/uploads/button-png/red-button-circle-image-pixabay-20.png' style={{ width: 400, height: 400 }} /> </a> : null}
+          {click === 8 ? <h1> Please select giant red button to proceed</h1> : null}
 
-      </div>
+        </div>
       </div>
     );
   }
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,28 @@
 const path = require("path");
 const router = require("express").Router();
 const userRoutes = require("./user");
+const Score = require("../../models/score")
 
-router.use("/users",userRoutes);
+router.use("/users", userRoutes);
+
+router.post("/score", function (req, res) {
+  console.log(Score);
+  console.log(req.body)
+  Score.create(req.body, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+      res.send("score recieved")
+    }
+  })
+})
 
 // If no API routes are hit, send the React app
 router.use((req, res) =>
-  res.sendFile(path.join(__dirname,"../../client/build/index.html"))
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"))
 );
+
+
 
 module.exports = router; 
