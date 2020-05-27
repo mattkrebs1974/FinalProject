e99a12311649d8c208924066bb149373da42af93
Jacob Gold <jkobygold@gmail.com>
2020-05-27 19:39:26 -0400

    making instructions page on a set interval

--- client/src/pages/Instructions.page.js
+++ client/src/pages/Instructions.page.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
 import "./instructions.css"
 class Instructions extends Component {
-    state = {  }
+        constructor(props) {
+          super(props);
+          this.state = {
+            hidden: false,
+          };
+          
+        }
+
+        
+          componentDidMount(){
+            var timeout = setTimeout(() => {
+                this.setState((state) => ({
+                  hidden: true,
+                }));
+              }, 10000);
+            }
+
+        
     render() { 
-        return ( <>
-           
-          <h1> Instructions:</h1>
-         <p>The following game is designed to test your performanceThe following game is designed to test your performanceThe following game is designed to test your performanceThe following game is designed to test your performance</p>
-         <a href="/Form"> <img src='https://www.kindpng.com/picc/m/46-468918_brain-clipart-draw-aesthetic-brain-drawing-hd-png.png' alt="" style={{ width: 100, height: 100 }} /> </a>
-                 </> );
+        return ( 
+         <>
+          <h1  style={{ 
+            visibility:this.state.hidden ? "hidden" : "visible"
+       }}
+        > Instructions:</h1>
+         <div  
+         style={{ 
+             visibility:this.state.hidden ? "hidden" : "visible"
+        }} > 
+         <p> This text is going to dissapear in ten seconds
+        </p>
+       </div>
+         
+       
+       <a href="/Form"> <img src='https://www.kindpng.com/picc/m/46-468918_brain-clipart-draw-aesthetic-brain-drawing-hd-png.png' alt="" 
+         style={{ 
+             width: 200,
+            height: 200 
+        }} />   
+       
+        </a>
+         </> );
     }
 }
  
