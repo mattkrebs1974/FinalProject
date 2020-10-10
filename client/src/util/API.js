87e55120468002830da627566b2f977782e2ba9b
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-10-09 22:01:47 -0400

    added error message

--- client/src/util/API.js
+++ client/src/util/API.js
@@ -1,38 +1,34 @@
 import axios from "axios";
 
-export default  {
+export default {
   // Saves a user to the database
   saveUser: function (userData) {
     return axios.post("/api/users", userData);
-
   },
 
   sendSurvey: function (surveyData) {
     return axios.post("/api/survey", surveyData);
-
-
   },
 
   getUsers: function () {
     return axios.get("/api/users");
   },
 
-  login: function (user) {
-    return axios.post("/api/users/login", user);
+  login: function (err, user) {
+    if (err) {
+      console.log("HIIIII", err);
+    } else {
+      return axios.post("/api/users/login", user);
+    }
   },
-
 
   performancedata: function (data) {
     console.log("performance it is");
-    return axios.post("/api/data/performance",data);
+    return axios.post("/api/data/performance", data);
   },
-
 
   gameData: function (data) {
     console.log("Game it is");
-    return axios.post("/api/game",data);
+    return axios.post("/api/game", data);
   },
 };
-
-
- 
